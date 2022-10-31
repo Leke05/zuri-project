8f83abc1d086609776f3b1b308a6c1cefba738be
Leke05 <babatundejonathan05@gmail.com>
2022-10-31 13:39:00 +0100

    commit

---
+++ src/component/Profile.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Box, Button, Heading,  Image, Flex, Text} from '@chakra-ui/react'
+import {BsTwitter} from 'react-icons/bs'
+
+const Profile = () => {
+  return (
+    <Box minW={'80vw'} bg={"#FFFFFF"}  alignItems={"center"}  flexDir={{base: "column", md: "row", lg: "row"}} m={"auto"}>
+
+     <Image src={"./images/picture1.png"} boxSize={"100px"} borderRadius={"20px"} m={ "auto"} my={"20px"}/>
+        <Heading fontSize={'12pxs'} textAlign={"center"} mb={"20px"}>@Jonathan</Heading>
+     <Box w={"85%"}  m={"auto"}>
+        <Box display={'flex'} flexDir={"column"} gap={5}>
+        <Button leftIcon={<BsTwitter/>}  bg={"#E6E8EC"}><a target="_blank" href="https://twitter.com/_Jonathan_0199">Twitter</a></Button>
+        <Button bg={"#E6E8EC"}><a target="_blank" href="https://training.zuri.team/">Zuri Team</a></Button>
+        <Button bg={"#E6E8EC"}><a target="_blank" href="http://books.zuri.team">Zuri Books</a></Button>
+        <Button bg={"#E6E8EC"}><a target="_blank" href="http://books.zuri.team/python">Python Books</a></Button>
+        <Button bg={"#E6E8EC"}><a target="_blank" href="https://background.zuri.team">Background Coder</a></Button>
+        <Button bg={"#E6E8EC"}><a target="_blank" href="https://books.zuri.team/design-rules">Design Books</a></Button>
+        </Box>
+        <Image src={"./images/Footer.png"} m={ "auto"}/>
+    </Box>
+
+    <hr style={{backgroundColor: "#FFFFFF", color: "#FFFFFF"}}/>
+
+    <Flex justify={"space-between"} flexDir={{base: "column", md: "row", lg: "row"}} gap={{base: "20px", md: "1px", lg: "1px"}} m={"auto"} w={"85%"} mb={{base: "10px", md: "1px", lg: "1px"}} mt={"40px"}>
+        <Image src={"./images/Vector.png"} w={"150px"}/>
+        <Text>HNG Intership 9 Frontend Task</Text>
+        <Image src={"./images/I4G.png"}  w={"150px"}/>
+    </Flex>
+
+    </Box>
+  )
+}
+
+export default Profile 
